cd468f79afeb6f3c2abb4c5d0acb1cbb75e8fa94
Bob Paulin <bob@bobpaulin.com>
2018-10-11 16:38:11 -0500

    Adding greeting path.

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -1,6 +1,12 @@
 package com.example;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,8 +17,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySpringBootRouter extends RouteBuilder {
 
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+	
     @Override
     public void configure() {
+    	
+    	
+    	restConfiguration().component("netty4-http").port(8880).bindingMode(RestBindingMode.json);
+    	
+    	rest("/greetinig").get()
+			.outType(Greeting.class)
+			.to("direct:greeting");
+    	
+    	from("direct:greeting")
+		.process(new Processor() {
+			
+			public void process(Exchange exchange) throws Exception {
+				String name = exchange.getIn().getHeader("name", String.class);
+				
+				exchange.getIn().setBody(new Greeting(counter.incrementAndGet(),
+                        String.format(template, name)));
+			}
+		});
+    	
         from("timer:hello?period={{timer.period}}").routeId("hello")
             .transform().method("myBean", "saySomething")
             .filter(simple("${body} contains 'foo'"))
